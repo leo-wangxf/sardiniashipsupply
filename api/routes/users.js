1c706f1e86cd5d227b854f654aec85009c0f49f4
Davide <davide85@gmail.com>
2016-08-01 16:46:34 +0200

    Added edit user profile

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var extend = require('util')._extend;
 var router = express.Router();
 var _ = require('underscore')._;
 var au = require('audoku');
+var Promise = require('bluebird');
 var User = require('../models/users').User;
 var tu = require('../util/token');
+
+// creo una copia di joi in modo da poterla promisificare 
+// senza intaccare quella usata negli altri file
+var Joi = _.extend({}, require('joi'));
+Joi.validate = Promise.promisify(Joi.validate, Joi);
 
 router.get('/',
   au.doku({  // json documentation
@@ -62,6 +69,142 @@ router.get('/',
       return res.boom.badImplementation(err); // Error 500
     });
 });
+
+
+router.put('/',
+  au.doku({  // json documentation
+    description: 'Update the user profile',
+    fields: 
+    {
+      name: 
+      {
+        description: 'The user name',
+        type: 'string', required: false
+      },
+      address: 
+      {
+        description: 'The user\'s address (only for suppliers)',
+        type: 'string', required: false
+      },
+      logo: 
+      {
+        description: 'The url of the user\'s logo (only for suppliers)',
+        type: 'string', required: false
+      },
+      phone: 
+      {
+        description: 'The user\' phone number',
+        type: 'integer', required: false
+      },
+      description: 
+      {
+        description: 'The business description (only for suppliers)',
+        type: 'string', required: false
+      },
+      web: 
+      {
+        description: 'URL of user\'s website (only for suppliers)',
+        type: 'string', required: false
+      },
+      favoriteSupplier:
+      {
+        description: 'The list of the user\'s favorite supplier (only for customers)',
+        // TODO tipo list al posto di string
+        type: 'string', required: false
+      },
+      certifications:
+      {
+        description: 'The list of the user\'s certifications (only for suppliers)',
+        // TODO tipo object al posto di string
+        type: 'string', required: false
+      },
+      categories:
+      {
+        description: 'categories provided by the user (only for suppliers)',
+        // TODO tipo list al posto di string
+        type: 'string', required: false
+      },
+      pIva:
+      {
+        description: 'The user\'s VAT identification number (only for suppliers)',
+        type: 'string', required: false
+      }
+    }
+  }),
+  function (req, res) {
+   
+    var userToken = req.token;
+    var userVals;
+    var userId;
+
+    if(userToken == undefined)
+    {
+      return res.boom.forbidden("Missing token");
+    }
+
+    tu.decodeToken(userToken).then(function(result)
+    {
+      if(result.response.statusCode == 200 && result.body.valid == true)
+      {
+        userId = result.body.token._id;
+        var userType = result.body.token.type;
+
+        userVals = req.body.user;
+
+        var schemaOpt = 
+        {
+          name: Joi.string().alphanum().min(3),
+          address: Joi.string().min(3),
+          phone: Joi.number()
+        };
+
+        if(userType == "customer")
+        {
+          schemaOpt.favoriteSuppliers = Joi.array().items(Joi.string());
+        }
+        else if(userType == "supplier")
+        {
+          schemaOpt.logo = Joi.string().uri();
+          schemaOpt.description = Joi.string();
+          schemaOpt.web = Joi.string().uri();
+          schemaOpt.categories = Joi.array().items(Joi.number());
+          schemaOpt.pIva = Joi.number();
+        }
+
+        var schema = Joi.object().keys(schemaOpt);
+
+        return Joi.validate(userVals, schema);
+      }
+      else
+      {
+        var err = new Error();
+        err.message = result.body.error_message;
+        err.statusCode = result.response.statusCode;
+        return err;
+      }
+    }).then(function(done)
+    {
+      return tu.editUser(userId, userToken, req.body);
+    }).then(function(result)
+    {
+      return res.status(result.response.statusCode).send(result.body);
+    }).catch(function(err)
+    {
+      console.log(err);
+      if(err.statusCode)
+      {
+        return res.status(err.statusCode).send(result.body);
+      }
+      else
+      {
+        return res.boom.badImplementation(err); // Error 500
+      }
+    });
+  }
+);
+
+
+
 
 
 /* GET users listing. */
--- api/util/token.js
+++ api/util/token.js
@@ -3,7 +3,8 @@ var request = require('request');
 
 var msToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2RlIjoibXMiLCJpc3MiOiJub3QgdXNlZCBmbyBtcyIsImVtYWlsIjoibm90IHVzZWQgZm8gbXMiLCJ0eXBlIjoiYXV0aG1zIiwiZW5hYmxlZCI6dHJ1ZSwiZXhwIjoxNzg0NzI3MzU5MjU5fQ.YYzZ7GcQVBQhvHkEU0T2pOs0Uk4gIt6l-wGRfNgsM6M";
 
-var authUrl = "http://localhost:3005";
+var authUrl = "http://localhost:3007";
+var userUrl = "http://localhost:3008";
 
 
 function decodeToken(token)
@@ -31,7 +32,6 @@ function decodeToken(token)
         return reject(decodeError);
       }
 
-      
       var r = {};
       r.body = JSON.parse(body);
       r.response = response;
@@ -47,4 +47,56 @@ function decodeToken(token)
 }
 
 
+function editUser(uid, token, body)
+{
+  var options = 
+  {
+    url: userUrl + "/users/" + uid,
+    method: 'PUT',
+    json: true,
+    body: body,
+    headers: 
+    {
+      'Authorization': 'Bearer ' + token,
+      'content-type': 'application/json'
+    }
+  };
+
+  return new Promise(function(resolve, reject)
+  {
+    request.put(options, function(error, response, body)    
+    {
+      if(error)
+      {
+        const decodeError = new Error();
+        decodeError.message = error.message;
+        decodeError.stack = error.stack;
+        return reject(decodeError);
+      }
+
+      var r = {};     
+      try
+      {
+        r.body = JSON.parse(body);
+      }
+      catch(err)
+      {
+        r.body = body;
+      }
+
+      r.response = response;
+      return resolve(r);
+      /*
+      if(response.statusCode == 200 && body)
+      {
+        return resolve(body);
+      }
+      */
+    });
+  });
+}
+
+
+
 exports.decodeToken  = decodeToken;
+exports.editUser  = editUser;
