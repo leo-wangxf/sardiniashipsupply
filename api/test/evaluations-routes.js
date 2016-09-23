d05f4363c2272791249ecfcbd509073049675011
mlclemente <clem@crs4.it>
2016-09-23 16:04:59 +0200

    added some tests

--- api/test/evaluations-routes.js
+++ api/test/evaluations-routes.js
@@ -55,19 +55,26 @@ describe('Evaluation Routes', function () {
 
         var range = _.range(100);
 
+	function getRandom(min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
+	};
+	var token = function() {
+            return '008f4fdc09dd8c1c3e'+getRandom(10,99)+'d36'; // to 
+	};
+        var t = token() + '' + getRandom(0,9);
 
         var createUsers = function (callback) {
             async.each(range, function (e, cb) {
                 user = new User({
-                    id: "008f4fdc09dd8c1c3e51d364",
+                    id: t,
                     name: "Guest " + e,
                     address: "Maple street " + e,
                     password: "pw" + e
                 });
-
-                user.save(function (err, message) {
+                t = token() + '' + getRandom(0,9);
+		user.save(function(err, u) {
                     if (err) throw err;
-                    users.push(user._id);
+                    users.push(u.id);
                     cb();
 
                 });
@@ -104,8 +111,8 @@ describe('Evaluation Routes', function () {
         var createEvaluations = function () {
             async.each(range, function (e, cb) {
                 evaluation = new Evaluation({
-                    from: users[_.random(0, 99)],
-                    to: users[_.random(0, 99)],
+                    from: users[0],
+                    to: users[1],
                     conversationId: conversations[_.random(0, 99)],
                     overall_rate: 5.0,
                     delivery_rate: 5.0,
@@ -120,7 +127,14 @@ describe('Evaluation Routes', function () {
                     cb();
                 });
             }, function (err) {
-                done();
+		Evaluation.findOne({}, function(err, eval) {
+                  if (err) throw err;
+		  testEvaluationId = eval._id;
+		  testEvaluationFrom = eval.from;
+		  testEvaluationTo = eval.to;
+		  testOverallRate = eval.overall_rate;
+                  done();
+		});
             });
         };
 
@@ -158,13 +172,46 @@ describe('Evaluation Routes', function () {
         ], removeEvaluations);
 
     });
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// GET TESTS ////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    
+    describe('GET' + apiprefix + '/evaluations', function() {
+	    it('Checks if response status is equal to 200 when requesting all evaluations', function(done) {
+	            request.get(apihost + apiprefix + '/evaluations', function(error, response, body) {
+		    if (error) throw error;
+		    else {
+			    response.statusCode.should.be.equal(200);
+		    }
+                    done(); // meaning: current async test is completed
+	    });
+
+	    });
+    
+    });
+    describe('GET' + apiprefix + '/evaluations', function() {
+	    it('Must return all 100 test evaluations and pagination metadata', function(done) {
+	            request.get(apihost + apiprefix + '/evaluations', function(error, response, body) {
+		    if (error) throw error;
+		    else {
+			    response.statusCode.should.be.equal(200);
+			    var results = JSON.parse(body);
+			    results.should.have.property('total');
+			    results.total.should.be.equal(100);
+		    }
+		    done();
+	    });
+	    });
+    });
     describe('GET ' + apiprefix + '/evaluations?page=2&limit=30', function () {
 
-        it('Pagination test: must include metadata with correct values', function (done) {
+        it('Pagination test: must include pagination metadata with correct values', function (done) {
 
             request.get(apihost + apiprefix + '/evaluations?page=2&limit=30', function (err,response, body) {
                 if (err) throw err;
                 else {
+                    response.statusCode.should.be.equal(200);
                     var results = JSON.parse(body);
                     should.exist(results.docs);
                     results.docs.length.should.be.equal(30);
@@ -172,15 +219,100 @@ describe('Evaluation Routes', function () {
                     results.limit.should.be.equal(30);
                     results.should.have.property('total');
                     results.total.should.be.equal(100);
-
                 }
                 done();
-
             });
 
         });
-
     });
+
+
+    describe('GET' + apiprefix + '/evaluations', function() {
+	    it('Must return all 100 test evaluations with related values', function(done) {
+	            request.get(apihost + apiprefix + '/evaluations', function(error, response, body) {
+		    if (error) throw error;
+		    else {
+			    response.statusCode.should.be.equal(200);
+			    var results = JSON.parse(body);
+			    results.should.have.property('total');
+			    results.total.should.be.equal(100);
+		    }
+		    done();
+	    });
+	    });
+    });
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// POST TESTS ///////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    describe('POST' + apiprefix + '/evaluations', function() {
+	    it('must create an evaluation with required fields', function(done) {
+		var eval_fields = {
+                    from: users[_.random(0, 99)],
+                    to: users[_.random(0, 99)],
+                    conversationId: conversations[_.random(0, 99)],
+                    overall_rate: 5.0,
+                    delivery_rate: 5.0,
+                    product_rate: 5.0,
+                    overall_review: "wonderful product!",
+                    conversation_end_time: Date.now(),
+                    evaluation_time: Date.now()
+		};
+		var c = {
+			url: apihost + apiprefix + '/evaluations',
+			body: JSON.stringify(eval_fields),
+			headers: {'content-type': 'application/json'}
+		};
+		request.post(c, function(error, response, body) {
+			if(error) throw error;
+			else {
+				response.statusCode.should.be.equal(201);
+				var results = JSON.parse(body);
+				results.should.have.property('from');
+				mongoose.Types.ObjectId(results.from).id.should.be.equal(eval_fields.from.id);
+				results.should.have.property('to');
+				mongoose.Types.ObjectId(results.to).id.should.be.equal(eval_fields.to.id);
+				results.should.have.property('conversationId');
+				results.should.have.property('overall_rate');
+				results.overall_rate.should.be.equal(eval_fields.overall_rate);
+				results.should.have.property('delivery_rate');
+				results.delivery_rate.should.be.equal(eval_fields.delivery_rate);
+				results.should.have.property('product_rate');
+				results.product_rate.should.be.equal(eval_fields.product_rate);
+				results.should.have.property('overall_review');
+				results.overall_review.should.be.equal(eval_fields.overall_review);
+				results.should.have.property('conversation_end_time');
+                                new Date(results.conversation_end_time).toString().should.be.equal(new Date(eval_fields.conversation_end_time).toString());
+				results.should.have.property('evaluation_time');
+                                new Date(results.evaluation_time).toString().should.be.equal(new Date(eval_fields.evaluation_time).toString());
+
+				//console.log(results);
+			};
+			done();
+		});
+	    });
+    });
+
+    describe('POST' + apiprefix + '/evaluations', function() {
+	    it('must get bad data error because req.body is empty', function(done) {
+		var c = {
+			url: apihost + apiprefix + '/evaluations',
+			body: "",
+			headers: {'content-type': 'application/json'}
+		};
+		request.post(c, function(error, response, body) {
+			if (error) throw error;
+			else {
+				response.statusCode.should.be.equal(422); // bad data error
+			};
+			done();
+		});
+	    });
+    });
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// PUT TESTS ///////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
 
 });
 	    
