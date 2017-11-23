aab8128135382500832ed76860d3f6d0fb8d5e1f
davide.muroni <davide85@gmail.com>
2017-11-23 12:14:20 +0100

    Added AccessControlAllowOrigin option to config file (CORS setting)

--- api/app.js
+++ api/app.js
@@ -17,6 +17,8 @@ var messages = require('./routes/messages');
 var requests = require('./routes/requests');
 var evaluations = require('./routes/evaluations');
 var files = require('./routes/files');
+var config = require('propertiesmanager').conf;
+
 //var test = require('./routes/test');
 //var cors = require('cors');
 var app = express();
@@ -61,14 +63,11 @@ app.use(cookieParser());
 app.use(bearerToken());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(cors());
-//
+
 app.use(function(req, res, next) {
-  //res.header('Access-Control-Allow-Origin', req.header("host"));
-  //res.header('Access-Control-Allow-Origin',"http://seidue.crs4.it:3010");
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', config.AccessControlAllowOrigin);
   //res.header('Access-Control-Allow-Credentials' 'false');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-
   res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
   if ('OPTIONS' == req.method) res.sendStatus(200);
     else next();
