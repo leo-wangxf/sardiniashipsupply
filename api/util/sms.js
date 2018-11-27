4d154ab65b2780828add1949069c37116ff357db
davide.muroni <davide85@gmail.com>
2018-11-27 16:24:33 +0100

    copied comilio api inside our code

--- api/util/sms.js
+++ api/util/sms.js
@@ -1,4 +1,5 @@
-const SMS = require('node-sms-send')
+//const SMS = require('node-sms-send')
+const SMS = require('./comilio_nostro')
 const config = require('propertiesmanager').conf;
 
 const sms = new SMS(config.smsUsername, config.smsPassword);
@@ -6,7 +7,7 @@ const sms = new SMS(config.smsUsername, config.smsPassword);
 
 function sendSMS(phoneNumber, message)
 {
-  return sms.send(phoneNumber, message);
+  return sms.send(phoneNumber, message, "Smart", "+393313106278");
 }
 
 exports.send = sendSMS;
